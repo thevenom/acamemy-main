376735e98dc43fa885138c7585d072475fb2b763
thevenom <imbu26@naver.com>
2023-06-16 09:42:04 +0900

    0616_0941

---
+++ pybo/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Question
+from django.utils import timezone
+from .forms import QuestionForm
+
+# Create your views here.
+
+def index(request):
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    # return HttpResponse("안녕하세요. pybodp 오신것을 환영합니다")
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    pybo 내용 출략
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    """
+    pybo 답변 등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'),
+                               create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
+
+def question_create(request):
+    """
+    pybo 질문 등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
